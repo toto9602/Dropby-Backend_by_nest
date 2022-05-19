3f7cf29fe174b77be3b3fa3ad93e0caeab216c45
toto9602 <toto9602@naver.com>
2022-05-20 01:23:41 +0900

    [feat]: DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { DropModule } from './drop/drop.module';
+import { UserModule } from './user/user.module';
+import { AreaModule } from './area/area.module';
+import { PlaceModule } from './place/place.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(),
+            DropModule,
+            UserModule,
+            AreaModule,
+            PlaceModule
+          ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(private connection:Connection) {}
+}
